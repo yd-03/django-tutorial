241888c6548efb9f20bfd20fe8fb8534f420df94
yd-03 <3nu4ydye57@gmail.com>
2024-08-23 08:01:13 +0900

    feat: Book modelを追加

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """
+    書籍モデル
+    """
+
+    class Meta:
+        # テーブル名を定義
+        db_table = "book"
+
+    # フィールドを定義
+    title = models.CharField(
+        verbose_name="タイトル", max_length=255, unique=True
+    )  # verbose_nameはフィールドのラベル, max_lengthは最大文字数, uniqueは一意制約
+    price = models.IntegerField(
+        verbose_name="価格", null=True, blank=True
+    )  # nullはデータベースにNULLを許可するかどうか, blankはフォームの入力欄に空欄を許可するかどうか
+
+    def __str__(self):
+        return self.title
